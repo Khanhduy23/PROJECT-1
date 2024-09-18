fab90ef762d608fb44040f07e2ce25afb1fcf1cc
Khanh Duy <lukiluna23803@gmail.com>
2024-09-18 23:58:29 +0700

    check

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const baseUrl = window.location.pathname.includes('/project-html/') ? '../' : '';
+
     // Load header
-    fetch('../header.html')
+    fetch(`${baseUrl}header.html`)
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
+
+            // Cập nhật các liên kết trong header
+            const links = document.querySelectorAll('header a');
+            links.forEach(link => {
+                link.href = baseUrl + link.getAttribute('href');
+            });
 
             // Sau khi header đã được tải, gắn các sự kiện cho menu toggle
             const menuToggle = document.querySelector('.menu-toggle');
@@ -17,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
     // Load footer
-    fetch('../footer.html')
+    fetch(`${baseUrl}footer.html`)
         .then(response => response.text())
         .then(data => {
             document.querySelector('footer').innerHTML = data;
