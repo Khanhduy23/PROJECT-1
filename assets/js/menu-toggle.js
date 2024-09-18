812491d9a1bbc261312f0e258b0d13e01a41e076
Khanh Duy <lukiluna23803@gmail.com>
2024-09-18 10:36:11 +0700

    Initial commit

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Load header
-    fetch('../header.html')
+    fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
     // Load footer
-    fetch('../footer.html')
+    fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('footer').innerHTML = data;
